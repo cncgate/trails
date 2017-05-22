0d1b80a2834d40eb5186f601034b1635d098a2f8
Dmitry Smirnov <dmitrijs.smirnovs@diatomenterprises.com>
2017-05-22 10:35:56 +0300

    Runner API controller

--- Controllers/RunnerController.cs
+++ Controllers/RunnerController.cs
@@ -4,39 +4,47 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using trails.Models;
 
 namespace trails.Controllers
 {
-    [Produces("application/json")]
+    //[Produces("application/json")]
     [Route("api/[controller]")]
     public class RunnerController : Controller
     {
         // GET: api/Runner
         [HttpGet]
-        public IEnumerable<string> Get()
+        public Runner[] Get()
         {
-            return new string[] { "value1", "value2" };
+            var runners = new List<Runner>();
+            runners.Add(new Runner() { Id = 1, First = "Dmitry", Last = "Smirnov", Age = 34, FavoriteItem = "Motorsport" });
+            runners.Add(new Runner() { Id = 2, First = "Alex", Last = "Popov", Age = 38, FavoriteItem = "Gym" });
+            runners.Add(new Runner() { Id = 3, First = "Arthur", Last = "Smulko", Age = 24, FavoriteItem = "Ice Hockey" });
+
+            return runners.ToArray();
         }
 
         // GET: api/Runner/5
         [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        public Runner Get(int id)
         {
-            return "value";
+            return Get()
+                    .Where(r => r.Id == id)
+                    .FirstOrDefault();
         }
-        
+
         // POST: api/Runner
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
-        
+
         // PUT: api/Runner/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
-        
+
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
---
+++ Models/Runner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace trails.Models
+{
+    public class Runner
+    {
+        public int Id { get; set; }
+        public string First { get; set; }
+        public string Last { get; set; }
+        public int Age { get; set; }
+        public string FavoriteItem { get; set; }
+
+
+        public Runner()
+        {
+        }
+    }
+}
