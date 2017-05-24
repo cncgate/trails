7d5c55d6a32f39b201b40052c2e0060aeead27c0
Dmitry Smirnov <dmitrijs.smirnovs@diatomenterprises.com>
2017-05-24 13:28:44 +0300

    Integrating JavaScript Frameworks (angular)

---
+++ wwwroot/app/home.controller.js
@@ -0,0 +1,18 @@
+﻿
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('HomeController', ['$scope', HomeController]);
+
+    function HomeController($scope) {
+        var vm = this;
+
+        vm.title = '';
+
+        activate();
+
+        function activate() { }
+    }
+})();
---
+++ wwwroot/app/runners.controller.js
@@ -0,0 +1,27 @@
+﻿
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('RunnersController', ['$scope', RunnersController]);
+
+    RunnersController.$inject = ['$http'];
+
+    function RunnersController($scope) {
+        var vm = this;
+
+        vm.title = '';
+
+        activate();
+
+        function activate() {
+            $http.get('/api/runner').then(function (response) {
+                vm.runners = response.data;
+
+            }).catch(function (err) {
+                console.log(err);
+            })
+        }
+    }
+})();
