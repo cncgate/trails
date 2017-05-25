1cf63b0117d3bd830d4f822e31b2a7f2673cae5d
Dmitry Smirnov <dmitrijs.smirnovs@diatomenterprises.com>
2017-05-25 10:06:00 +0300

    syntax error in runners controller

--- wwwroot/app/runners.controller.js
+++ wwwroot/app/runners.controller.js
@@ -3,7 +3,7 @@
 
     angular
         .module('app')
-        .controller('RunnersController', RunnersController]);
+        .controller('RunnersController', RunnersController);
 
     RunnersController.$inject = ['$http'];
 
